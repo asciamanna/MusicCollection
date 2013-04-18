e651f74b18af6701f0f6b0fa4304345a9257a258
Anthony Sciamanna <asciamanna@gmail.com>
2013-04-18 17:46:43 -0400

    cleaned up javascript

--- MusicCollection/Scripts/musiccollection.js
+++ MusicCollection/Scripts/musiccollection.js
@@ -2,35 +2,29 @@
     var num_pages = $('.pagination').children('ul').children('li').toArray().length - 2; //substract 2 for previous and current
     $('.pagination').children('ul').children('li').each(function (index, element) {
         var anchor = $(element).children('a').first();
-        var current_page = anchor.data('currentpage');
+        var current_page = parseInt(anchor.data('currentpage'));
 
-        if (anchor.html() == "Prev") {
-            if (current_page == "1") {
+        if (anchor.html() === "Prev") {
+            if (current_page === 1) {
                 $(element).addClass("disabled");
             }
             else {
-                anchor.attr("href", "Album?page=" + (parseInt(current_page) - 1));
+                anchor.attr("href", "Album?page=" + (current_page - 1));
             }
         }
-        
-        if (current_page == anchor.html()) {
+
+        if (current_page === parseInt(anchor.html())) {
             $(element).addClass("active");
         }
 
-        if (anchor.html() == "Next") {
-            if (current_page == num_pages) {
+        if (anchor.html() === "Next") {
+            if (current_page === num_pages) {
                 $(element).addClass("disabled");
             }
             else {
-                anchor.attr("href", "Album?page=" + (parseInt(current_page) + 1));
+                anchor.attr("href", "Album?page=" + (current_page + 1));
             }
         }
-
     });
-
     $('#navbar').affix();
 });
-
-//$('#albumArtworkCarousel').bind('slid', function () {
-//    activeId = $('.active.item').children('p[hidden]').first().attr('title');
-//});
